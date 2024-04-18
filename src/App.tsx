349aabb057efd59014b45dd1c3512c8f974117c7
Justina Ominisan <justinaominisan@gmail.com>
2024-04-18 14:29:37 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+
+
+const App: React.FC = () => {
+  return (
+    <>
+    <Navbar />
+    </>
+  
+  );
+}
+
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FaCartPlus } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { CiSearch } from "react-icons/ci";
+import { CiHeart } from "react-icons/ci";
+
+const Navbar: React.FC = () => {
+  return <>
+    <div id="navigation-bar">
+      <h1>Hello</h1>
+      <nav>
+        <ul>
+          <li><a href="#" id="home">Home</a></li>
+          <li><a href="#" id="shop">Shop</a></li>
+          <li><a href="#" id="about">About</a></li>
+          <li><a href="#" id="contact">Contact</a></li>
+
+          <li><a href="#" id="logo1"><CgProfile /></a></li>
+          <li><a href="#" id="logo2"><CiSearch /></a></li>
+          <li><a href="#" id="logo3"><CiHeart /></a></li>
+          <li><a href="#" id="logo4"><FaCartPlus /></a></li>
+        </ul>
+      </nav>
+    </div>
+  </>;
+}
+
+
+
+export default Navbar;
